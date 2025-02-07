6268d443c1696bb0ee5e4861dba0c011f66bb5ac
Luiz Carlos Oliveira Maciel <luiz.carlos@propig.com.br>
2025-02-07 13:09:00 -0300

    [ FEAT ] -> Setup new project with tern and sqlc

---
+++ 5-module/cmd/terndotenv/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"os/exec"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		fmt.Println("Erro ao carregar o arquivo .env:", err)
+		panic(err)
+	}
+
+	cmd := exec.Command(
+		"tern",
+		"migrate",
+		"--migrations",
+		"./internal/store/pgstore/migrations",
+		"--config",
+		"./internal/store/pgstore/migrations/tern.conf",
+	)
+
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Println("Comand execution failed: ", err)
+		fmt.Println("Output", string(output))
+		panic(err)
+	}
+
+	fmt.Println("Comand executed sucessfully ", string(output))
+}
